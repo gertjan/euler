3924d5509027466ce809b903c8daaba74b911015
Gertjan Beheydt <gertjan.beheydt+git@gmail.com>
2014-04-10 20:28:08 +0200

    Solve problem 6.

---
+++ 006.go
@@ -0,0 +1,10 @@
+package euler
+
+import "fmt"
+
+func P006() string {
+	limit := 100
+	sum := limit * (limit + 1) / 2
+	ssq := (2*limit + 1) * (limit + 1) * limit / 6
+	return fmt.Sprintf("%v", sum*sum-ssq)
+}
