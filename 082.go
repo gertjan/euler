167ee22d254a767b32c63022f8e4f9a816157ec1
Gertjan Beheydt <gertjan.beheydt+git@gmail.com>
2014-05-07 22:01:02 +0200

    Solve problem 82.

---
+++ 082.go
@@ -0,0 +1,64 @@
+package euler
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	//"sort"
+	"strconv"
+)
+
+func P082(in io.Reader) string {
+	r := csv.NewReader(in)
+	records, _ := r.ReadAll()
+	dim := len(records)
+
+	minCost := 0
+	tab := make([][]int, dim)
+	for i := 0; i < dim; i++ {
+		tab[i] = make([]int, dim)
+		for j := 0; j < dim; j++ {
+			v, _ := strconv.Atoi(records[i][j])
+			tab[i][j] = v
+			if v < minCost {
+				minCost = v
+			}
+		}
+	}
+
+	current := make([]int, dim)
+	for i := 0; i < dim; i++ {
+		current[i] = tab[i][0]
+	}
+
+	for col := 1; col < dim; col++ {
+		//go one col to the right
+		for i, path := range current {
+			path += tab[i][col]
+			current[i] = path
+		}
+		//optimize
+		for changed := true; changed; {
+			changed = false
+			for i, path := range current {
+				if up := i - 1; up >= 0 && current[up] > tab[up][col]+path {
+					changed = true
+					current[up] = tab[up][col] + path
+				}
+
+				if down := i + 1; down < dim && current[down] > tab[down][col]+path {
+					changed = true
+					current[down] = tab[down][col] + path
+				}
+			}
+		}
+	}
+
+	minPath := current[0]
+	for _, cp := range current {
+		if cp < minPath {
+			minPath = cp
+		}
+	}
+	return fmt.Sprintf("%v", minPath)
+}
---
+++ 082_test.go
@@ -0,0 +1,7 @@
+package euler
+
+import "testing"
+
+func TestP082(t *testing.T) {
+	testWithInput(t, P082, "260324")
+}
