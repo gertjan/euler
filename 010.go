3fd558bf1915e6a4b306e63d000208f018eaca5a
Gertjan Beheydt <gertjan.beheydt+git@gmail.com>
2014-05-07 21:11:33 +0200

    Solve problem 10.

---
+++ 010.go
@@ -0,0 +1,16 @@
+package euler
+
+import (
+	"fmt"
+)
+
+func P010() string {
+	_ = isPrime(2000000)
+	sum := 5
+	for i := 5; i < 2000000; i += 2 {
+		if isPrime(i) {
+			sum += i
+		}
+	}
+	return fmt.Sprintf("%v", sum)
+}
---
+++ 010_test.go
@@ -0,0 +1,9 @@
+package euler
+
+import (
+	"testing"
+)
+
+func TestP010(t *testing.T) {
+	test(t, P010, "142913828922")
+}
