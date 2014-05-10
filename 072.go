d287d604bca2302f59ca2cf6f34adb6bacbd4b5b
Gertjan Beheydt <gertjan.beheydt+git@gmail.com>
2014-05-10 08:57:50 +0200

    Solve problem 72.

---
+++ 072.go
@@ -0,0 +1,23 @@
+package euler
+
+import "fmt"
+
+func P072() string {
+	//init
+	limit := 1000001
+	phi := make([]int, limit)
+	for i, _ := range phi {
+		phi[i] = i
+	}
+
+	result := 0
+	for i := 2; i < limit; i++ {
+		if phi[i] == i {
+			for j := i; j < limit; j += i {
+				phi[j] = phi[j] / i * (i - 1)
+			}
+		}
+		result += phi[i]
+	}
+	return fmt.Sprintf("%v", result)
+}
---
+++ 072_test.go
@@ -0,0 +1,7 @@
+package euler
+
+import "testing"
+
+func TestP072(t *testing.T) {
+	test(t, P072, "303963552391")
+}
