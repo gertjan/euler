81ae88224a9aeb38b5940cff6c687195bfc5aed1
Gertjan Beheydt <gertjan.beheydt+git@gmail.com>
2014-05-14 20:27:02 +0200

    Solve problem 25.

---
+++ 025.go
@@ -0,0 +1,16 @@
+package euler
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func P025() string {
+	a,b := big.NewInt(1), big.NewInt(1)
+	i := 1
+	for len(a.String()) < 1000 {
+		a, b = b, a.Add(a,b)
+		i++
+	}
+	return fmt.Sprintf("%v", i)
+}
---
+++ 025_test.go
@@ -0,0 +1,7 @@
+package euler
+
+import "testing"
+
+func TestP025(t *testing.T) {
+	test(t, P025, "4782")
+}
